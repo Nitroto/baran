d639adf7722680ca644b71f66f81753db0f670a7
Asen Todorov <astodorov@outlook.com>
2021-12-14 19:07:53 +0200

    frontend init

--- backend/baran/settings/base.py
+++ backend/baran/settings/base.py
@@ -141,3 +141,7 @@ USER_SYSTEM_USERNAME = 'baran-system'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
