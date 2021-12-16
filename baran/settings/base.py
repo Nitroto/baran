be20c2dab8c6511f356176fb7527caeddcb9c3f6
asen_todorov <asen@modondo.com>
2021-12-16 16:17:06 +0200

    fix staticfiles for Heroku

--- baran/settings/base.py
+++ baran/settings/base.py
@@ -127,7 +127,7 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../staticfiles'))
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
